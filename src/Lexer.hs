681f2aa3947c66749f0c149948d57de8ce5f4e92
Alex Jercan <jercan_alex27@yahoo.com>
2023-06-02 18:49:57 +0300

    wip

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -2,47 +2,49 @@
 
 module Lexer where
 
-import Data.Text qualified as T
+import Data.Either (fromRight)
 import Data.Text (Text)
+import Data.Text qualified as T
 import Data.Void (Void)
 import Text.Megaparsec (MonadParsec (takeWhileP), Parsec, anySingle, choice, eof, many, manyTill, parse, (<|>))
 import Text.Megaparsec.Char (alphaNumChar, char, lowerChar, space1, upperChar)
 import Text.Megaparsec.Char.Lexer qualified as L
 import Token
-import Data.Either (fromRight)
 
 type Parser = Parsec Void Text
 
 tokenize :: Tokenizer
 tokenize = fromRight [] . parse tokensP "" . T.pack
-    where tokensP = sc *> many nextToken <* eof
+  where
+    tokensP = sc *> many nextToken <* eof
 
 nextToken :: Parser Token
-nextToken = choice
-    [ Dot <$ symbol "."
-    , At <$ symbol "@"
-    , Results <$ symbol "=>"
-    , Assign <$ symbol "<-"
-    , Tilde <$ symbol "~"
-    , Plus <$ symbol "+"
-    , Minus <$ symbol "-"
-    , Asterisk <$ symbol "*"
-    , Slash <$ symbol "/"
-    , Equal <$ symbol "="
-    , LessEqual <$ symbol "<="
-    , LessThan <$ symbol "<"
-    , SemiColon <$ symbol ";"
-    , Colon <$ symbol ":"
-    , Comma <$ symbol ","
-    , LeftParen <$ symbol "("
-    , RightParen <$ symbol ")"
-    , LeftSquirly <$ symbol "{"
-    , RightSquirly <$ symbol "}"
-    , Integer <$> numberP
-    , String  <$> stringP
-    , Type <$> typeP
-    , stringToToken <$> identP
-    ]
+nextToken =
+    choice
+        [ Dot <$ symbol "."
+        , At <$ symbol "@"
+        , Results <$ symbol "=>"
+        , Assign <$ symbol "<-"
+        , Tilde <$ symbol "~"
+        , Plus <$ symbol "+"
+        , Minus <$ symbol "-"
+        , Asterisk <$ symbol "*"
+        , Slash <$ symbol "/"
+        , Equal <$ symbol "="
+        , LessEqual <$ symbol "<="
+        , LessThan <$ symbol "<"
+        , SemiColon <$ symbol ";"
+        , Colon <$ symbol ":"
+        , Comma <$ symbol ","
+        , LeftParen <$ symbol "("
+        , RightParen <$ symbol ")"
+        , LeftSquirly <$ symbol "{"
+        , RightSquirly <$ symbol "}"
+        , Integer <$> numberP
+        , String <$> stringP
+        , Type <$> typeP
+        , stringToToken <$> identP
+        ]
 
 numberP :: Parser Integer
 numberP = lexeme L.decimal
--- test/Main.hs
+++ test/Main.hs
@@ -1,7 +1,12 @@
 module Main (main) where
 
-import LexerTest (tests)
-import Test.Hspec (hspec)
+import Lexer qualified
+import LexerTest (lexerTests)
+import Test.Hspec (Spec, describe, hspec)
+
+tests :: Spec
+tests = do
+    describe "Lexer" $ lexerTests Lexer.tokenize
 
 main :: IO ()
 main = hspec $ do
